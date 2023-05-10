e9b7f5f82b6cb9a58f791b5940a01471f8f82948
Yasin A. Yokus <ayokus@icloud.com>
2023-05-11 00:58:20 +0300

    add day 6

---
+++ q06.ts
@@ -0,0 +1,34 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+    let value = init;
+    nums.forEach((elem, i) => { value = fn(value, elem)});
+    return value;
+};
+
+
+
+// Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+
+// A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The final value of val is returned.
+
+// If the length of the array is 0, it should return init.
+
+// Please solve it without using the built-in Array.reduce method.
+
+ 
+
+// Example 1:
+
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr; }
+// init = 0
+// Output: 10
+// Explanation:
+// initially, the value is init=0.
+// (0) + nums[0] = 1
+// (1) + nums[1] = 3
+// (3) + nums[2] = 6
+// (6) + nums[3] = 10
+// The final answer is 10.
