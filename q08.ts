52fbb2c4b0b5e47d422abbc39ef20aa51e86b235
Yasin A. Yokus <ayokus@icloud.com>
2023-05-12 03:33:57 +0300

    add day 8 -- closures, closures everywhere

---
+++ q08.ts
@@ -0,0 +1,34 @@
+function once<T extends (...args: any[]) => any>(fn: T): 
+ ((...args: Parameters<T>) => ReturnType<T> | undefined) {
+  let isCalled = false;
+  return function (...args) {
+    if (isCalled) return;
+    isCalled = true;
+    return fn(...args)
+  };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
+
+
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+//     The first time the returned function is called, it should return the same result as fn.
+//     Every subsequent time it is called, it should return undefined.
+
+ 
+
+// Example 1:
+
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
